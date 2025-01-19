d3e1547c5f7886eab772ed4f5110d494bbe22e41
黄俊（Huang Jun） <SepComet@users.noreply.github.com>
2025-01-19 10:33:07 +0800

    update work project
    完成联机房间，不依靠UE的联机功能也能够实现局域网联机。

---
+++ Work/Source/Work/LoadingGameMode.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LoadingGameMode.h"
+
+ALoadingGameMode::ALoadingGameMode()
+{
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(
+		TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter.BP_FirstPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
+
+void ALoadingGameMode::HostLanGame()
+{
+	GetWorld()->ServerTravel("/Game/FirstPerson/Maps/FirstPersonMap.FirstPersonMap?Listen");
+}
+
+void ALoadingGameMode::JoinLanGame(FString address)
+{
+	APlayerController* LocalPlayerController = GetGameInstance()->GetFirstLocalPlayerController();
+	if (LocalPlayerController != NULL)
+	{
+		LocalPlayerController->ClientTravel(address, TRAVEL_Absolute);
+	}
+}
